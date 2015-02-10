80ca90470a42c399b04b065a70c8a656ad72b87b
laymard <Laurent.Aymard@insa-rennes.fr>
2015-02-10 09:52:51 +0100

    fin du cours

--- cours/cours3.ml
+++ cours/cours3.ml
@@ -1,5 +1,7 @@
 (*10/02/14*)
 (*LES TYPES*)
+open List;;
+open Graphics;;
 
 (*Types anonymes*)
 (4,true,1.0);;
@@ -9,5 +11,56 @@ type point = {x : float ; y : float};;
 let p1 = {x = 1.; y= 2.};;
 p1.x;;
 
-type point2 = int*int;;
-let p1 = (1,2);;
+let symdiag = fun { x=a ; y=b} -> {x=b ; y=a};;
+symdiag p1;;
+
+let symdiag2 p = {x=p.y ; y=p.x};;
+symdiag2 p1;;
+
+let symdiag3 = function
+  |{x=0.; y=b} -> {x=0. ; y=b}
+
+
+type cercle = {centre : point ; rayon :float};;
+
+let ycercle = function
+  |{centre = {x=0. ; y=_}; rayon =_} -> true
+  |_-> false;;
+
+(*Les types Sommes*)
+type direction = Nord | Sud | Est | Ouest;;
+type direction2 = 1|2; (*on ne peu pas mettre des valeurs ou des Types*)
+ (*Majuscules obligé!!!/*)
+let d = Nord;;
+let agauche = function
+  |Nord->Ouest
+  |Ouest -> Sud
+  |Sud ->Est
+  |Est-> Nord;;
+
+agauche Nord;;
+
+type personne = Nom of string | Nums of int;;
+let nom s = Nom s;;
+
+µom "Aµlan";;
+nom "Alan";;
+
+type couleur = Trefle|Carreau|Coeur|Pique;;
+type hauteur = As|Roi|Dame|Valet|Diw|Neuf|Huit|Sept;;
+type carte = Carte of hauteur * couleur ;;
+
+let is_valet c = match c with
+  |Carte(Valet,_) -> true
+  |_->false;;
+
+let _ =is_valet (Carte(Roi,Pique));;
+
+(*LES LISTES*)
+let f x = x*2;;
+(map f) (4::5::[]) ;;
+
+let rec map t =function
+  |[] -> []
+  |(a::reste) -> (t a):: map t reste;;
+
---
+++ cours/cours3.ml~
@@ -0,0 +1,13 @@
+(*10/02/14*)
+(*LES TYPES*)
+
+(*Types anonymes*)
+(4,true,1.0);;
+
+(*Types produits nommés*)
+type point = {x : float ; y : float};;
+let p1 = {x = 1.; y= 2.};;
+p1.x;;
+
+type point2 = int*int;;
+let p1 = (1,2);;
